5fdedb9f2d8ac46ea94c2ba1200d663a61e03970
jerzy <jerzy@IDEA330>
2024-05-30 13:17:33 +0200

    Dodaj pliki projektów.

---
+++ AngularApp1.Server/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngularApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+    }
+}
